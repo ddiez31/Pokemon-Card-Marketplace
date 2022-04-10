47129db2d2e144fa860817fad866bf794489c3a1
David Diez <david.diez31@free.fr>
2022-04-11 00:00:32 +0200

    feat(translate): configure translate to en and fr languages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { LANG } from '@models/lang.model';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+import { map, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Pokemon-Card-Marketplace';
+export class AppComponent implements OnInit, OnDestroy {
+  private _subscription = new Subscription();
+
+  constructor(private readonly titleService: Title, private readonly translateService: TranslateService) {
+    this.translateService.setDefaultLang(LANG.EN);
+    this.translateService.use(LANG.EN);
+  }
+
+  ngOnInit(): void {
+    this._subscription.add(
+      this.translateService.onLangChange.pipe(
+        map((event: LangChangeEvent) => {
+          this.translateService.reloadLang(event.lang);
+          this.translateService.get('app-name').subscribe(value => {
+            this.titleService.setTitle(value);
+          })
+      })).subscribe()
+    );
+  }
+
+  ngOnDestroy(): void {
+      this._subscription.unsubscribe();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,10 +9,20 @@ import { reducers, metaReducers } from './reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { EffectsModule } from '@ngrx/effects';
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import {MatButtonModule} from '@angular/material/button';
+
+export function createTranslateLoader(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
@@ -23,9 +33,19 @@ import { EffectsModule } from '@ngrx/effects';
       maxAge: 25,
       logOnly: environment.production
     }),
-    EffectsModule.forRoot([])
+    EffectsModule.forRoot([]),
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+          provide: TranslateLoader,
+          useFactory: (createTranslateLoader),
+          deps: [HttpClient]
+      }
+  }),
+
+  MatButtonModule
   ],
-  providers: [],
+  providers: [Title],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
