ba5ecf2efed2dafaa7ec778d07c1478081e110c9
David Diez <david.diez31@free.fr>
2022-04-13 00:39:22 +0200

    feat(card): add card component

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-
+import { SharedModule } from 'app/shared/shared.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
     HomeComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    FlexLayoutModule
   ]
 })
 export class HomeModule { }
